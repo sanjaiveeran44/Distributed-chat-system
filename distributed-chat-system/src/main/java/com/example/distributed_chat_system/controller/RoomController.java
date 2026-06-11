b93d1d7d652e81fb54abf5b876b0dacdce9c0dd1
sanjaiveeran44 <kit27.am44@gmail.com>
2026-06-11 13:25:55 +0530

    Roomcontroller updated

--- distributed-chat-system/src/main/java/com/example/distributed_chat_system/controller/RoomController.java
+++ distributed-chat-system/src/main/java/com/example/distributed_chat_system/controller/RoomController.java
@@ -1,24 +1,81 @@
 package com.example.distributed_chat_system.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.distributed_chat_system.dto.CreateRoomRequest;
+import com.example.distributed_chat_system.dto.RoomResponse;
+import com.example.distributed_chat_system.entity.Room;
+import com.example.distributed_chat_system.service.RoomService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
 
 @RestController
-@RequestMapping("/api/room")
+@RequestMapping("/api/rooms")
+@CrossOrigin(origins = "http://localhost:5173")
 public class RoomController {
-    @PostMapping("/create")
-    public void createRoom() {
-        System.out.println("Creating room");
+
+    private final RoomService roomService;
+
+    public RoomController(
+            RoomService roomService
+    ) {
+        this.roomService = roomService;
+        System.out.println("RoomController initialized");
     }
 
-    @GetMapping("/list")
-    public void listRooms() {
-        System.out.println("Listing rooms");
+    @PostMapping
+    public ResponseEntity<RoomResponse> createRoom(
+
+            @RequestBody CreateRoomRequest request,
+            Authentication authentication
+
+    ) {
+        System.out.println("===============Creating room=====================: " + request.getName());
+        System.out.println("===============Authentication: =================" + authentication.getName());
+
+        return ResponseEntity.ok(
+
+                roomService.createRoom(
+                        request,
+                        authentication
+                )
+
+        );
+
     }
 
-    @GetMapping("/{roomId}")
-    public void getRoom() {
-        System.out.println("Getting room");
+    @GetMapping
+    public ResponseEntity<List<Room>> getRooms() {
+
+        return ResponseEntity.ok(
+
+                roomService.getAllRooms()
+
+        );
+
     }
+
+    @PostMapping("/{roomId}/join")
+    public ResponseEntity<String> joinRoom(
+
+            @PathVariable Long roomId,
+            Authentication authentication
+
+    ) {
+
+        roomService.joinRoom(
+                roomId,
+                authentication
+        );
+
+        return ResponseEntity.ok(
+                "Joined Successfully"
+        );
+
+    }
+
 }
