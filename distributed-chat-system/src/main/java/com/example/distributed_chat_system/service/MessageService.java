2216c894613678328eea5dede494a4da571cd0c2
sanjaiveeran44 <kit27.am44@gmail.com>
2026-06-19 11:33:21 +0530

    Add Redis message publishing to MessageService

--- distributed-chat-system/src/main/java/com/example/distributed_chat_system/service/MessageService.java
+++ distributed-chat-system/src/main/java/com/example/distributed_chat_system/service/MessageService.java
@@ -6,21 +6,30 @@ import com.example.distributed_chat_system.repository.MessageRepository;
 import com.example.distributed_chat_system.repository.UserRepository;
 import com.example.distributed_chat_system.model.ChatMessage;
 import com.example.distributed_chat_system.entity.Message;
+import com.example.distributed_chat_system.redis.RedisPublisher;
 
 @Service
 public class MessageService {
 
     private final MessageRepository messageRepository;
     private final UserRepository userRepository;
+    private final RedisPublisher redisPublisher;
 
     public MessageService(
             MessageRepository messageRepository,
-            UserRepository userRepository){
+            UserRepository userRepository,
+            RedisPublisher redisPublisher
+            ){
         this.messageRepository = messageRepository;
         this.userRepository = userRepository;
+        this.redisPublisher = redisPublisher;
     }
 
     public Message saveMessage(Message dbMessage){
         return messageRepository.saveMessage(dbMessage);
+    }
+
+    public void publishMessage(ChatMessage message){
+        redisPublisher.publish(message);
     }
 }
