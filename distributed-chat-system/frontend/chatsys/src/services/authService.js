6d46e7fc15cf40f98acc18a63f6d7782b26f828f
sanjaiveeran44 <kit27.am44@gmail.com>
2026-06-19 23:04:17 +0530

    refactor: update authService API endpoint URL to remove port

--- distributed-chat-system/frontend/chatsys/src/services/authService.js
+++ distributed-chat-system/frontend/chatsys/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/api/auth";
+const API = "http://localhost/api/auth";
 
 export const register = async (userData) => {
     return await axios.post(`${API}/register`, userData);
